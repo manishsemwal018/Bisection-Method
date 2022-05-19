6dbd237ca303b8e59616d8376573513e80e43c3e
Manish Semwal <83582201+manishsemwal018@users.noreply.github.com>
2022-05-19 20:41:19 +0530

    Update bisection.c

--- bisection.c
+++ bisection.c
@@ -1,23 +1,34 @@
 #include<stdio.h>
+#include<conio.h>
 #include<math.h>
-#define f(x) x*x*x-4*x+1
+
+ 
+
+
+#define f(x) cos(x) - x * exp(x)
+
 void main()
 {
-	 double x0, x1, x2, y0, y1, y2, e;
+	 float x0, x1, x2, f0, f1, f2, e;
 	 int step = 1;
+	 clrscr();
+	
 	 up:
-	 printf("Enter two initial guesses:");
-	 scanf("%lf%lf", &x0, &x1);
+	 printf("\nEnter two initial guesses:\n");
+	 scanf("%f%f", &x0, &x1);
 	 printf("Enter tolerable error:\n");
-	 scanf("%lf", &e);
-	 y0 = f(x0);
-	 y1 = f(x1);
-	 if( y0 * y1 > 0.0)
+	 scanf("%f", &e);
+	
+	 f0 = f(x0);
+	 f1 = f(x1)
+	 
+	 if( f0 * f1 > 0.0)
 	 {
-		  printf("Guesse Incorrect.\n");
+		  printf("Incorrect Initial Guesses.\n");
 		  goto up;
 	 }
-     printf("\nStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
+ 
+	 printf("\nStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
 	 do
 	 {
 		  x2 = (x0 + x1)/2;
@@ -38,4 +49,5 @@ void main()
 		  step = step + 1;
 	 }while(fabs(f2)>e);
 	 printf("\nRoot is: %f", x2);
+	 getch();
 }
